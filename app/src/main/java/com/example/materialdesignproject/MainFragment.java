1bcc1aeed478e7fc2cf0f7755f095a91ce282bc3
mohammad <m.zandieh1999@gmail.com>
2021-05-11 23:25:11 +0430

    add setChecked to CardView

--- app/src/main/java/com/example/materialdesignproject/MainFragment.java
+++ app/src/main/java/com/example/materialdesignproject/MainFragment.java
@@ -10,12 +10,28 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.card.MaterialCardView;
+
 public class MainFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MaterialCardView materialCardView = view.findViewById(R.id.card_main_text);
+        materialCardView.setChecked(true);
+
+        materialCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                materialCardView.setChecked(!materialCardView.isChecked());
+            }
+        });
     }
 }
