1bdba6b59edb4c7144753819345743ab4cac0cac
mohammad <m.zandieh1999@gmail.com>
2021-05-12 01:09:17 +0430

    transaction.add(R.id.frame_main_fragmentContainer, new DetailFragment());

--- app/src/main/java/com/example/materialdesignproject/MainFragment.java
+++ app/src/main/java/com/example/materialdesignproject/MainFragment.java
@@ -9,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.card.MaterialCardView;
 
@@ -25,7 +26,18 @@ public class MainFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         MaterialCardView materialCardView = view.findViewById(R.id.card_main_text);
-        materialCardView.setChecked(true);
+        materialCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.add(R.id.frame_main_fragmentContainer, new DetalFragment());
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+
+//        materialCardView.setChecked(true);
 
 //        materialCardView.setOnClickListener(new View.OnClickListener() {
 //            @Override
