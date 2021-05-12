cf6a6b16d9484d236d3c6bc36dd21f6ddf0e57a0
mohammad <m.zandieh1999@gmail.com>
2021-05-12 19:06:55 +0430

    add setOnNavigationItemSelectedListener in MainActivity.java

--- app/src/main/java/com/example/materialdesignproject/MainActivity.java
+++ app/src/main/java/com/example/materialdesignproject/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.materialdesignproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,11 +17,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_main_fragmentContainer, new MainFragment());
-        transaction.commit();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView_main);
+        bottomNavigationView.setSelectedItemId(R.id.menuItemHome);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.menuItemHome:
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_main_fragmentContainer, new MainFragment());
+                        transaction.commit();
+                        break;
+
+                    case R.id.menuItemNearby:
+                        FragmentTransaction transaction2 = getSupportFragmentManager().beginTransaction();
+                        transaction2.replace(R.id.frame_main_fragmentContainer, new NearbyFragment());
+                        transaction2.commit();
+                        break;
+
+                    case R.id.menuItemRecents:
+                        FragmentTransaction transaction3 = getSupportFragmentManager().beginTransaction();
+                        transaction3.replace(R.id.frame_main_fragmentContainer, new RecentsFragment());
+                        transaction3.commit();
+                        break;
+
+                }
+                return true;
+            }
+        });
         bottomNavigationView.setSelectedItemId(R.id.menuItemHome);
 
     }
