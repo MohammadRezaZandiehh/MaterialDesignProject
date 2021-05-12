08adcc0bb067f8d0826e67f0665e196718363256
mohammad <m.zandieh1999@gmail.com>
2021-05-12 16:02:15 +0430

    add floatingActionButton.setClick in MainFragment.java

--- app/src/main/java/com/example/materialdesignproject/MainFragment.java
+++ app/src/main/java/com/example/materialdesignproject/MainFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainFragment extends Fragment {
 
@@ -41,6 +43,13 @@ public class MainFragment extends Fragment {
             }
         });
 
+        FloatingActionButton floatingActionButton = view.findViewById(R.id.fab_main);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(appCompatActivity, "Fab button is clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 //        materialCardView.setChecked(true);
 
