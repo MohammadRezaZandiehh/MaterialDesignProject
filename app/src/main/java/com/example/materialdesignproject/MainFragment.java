51d8293db001f1a944f393ab9398fd6263daa2c2
mohammad <m.zandieh1999@gmail.com>
2021-05-13 12:15:06 +0430

    add actionBarDrawerToggle & drawerLayout to MainActivity.java

--- app/src/main/java/com/example/materialdesignproject/MainFragment.java
+++ app/src/main/java/com/example/materialdesignproject/MainFragment.java
@@ -2,19 +2,24 @@ package com.example.materialdesignproject;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainFragment extends Fragment {
@@ -30,8 +35,16 @@ public class MainFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         Toolbar toolbar = view.findViewById(R.id.toolbar_main);
+        toolbar.setNavigationIcon(R.drawable.ic_menu_white_24dp);
+
         AppCompatActivity appCompatActivity = (AppCompatActivity) getActivity();
         appCompatActivity.setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawerLayout = view.findViewById(R.id.drawerLayout_main);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(getActivity(),
+                drawerLayout, toolbar, R.string.drawerOpen, R.string.drawerClose);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
 
         MaterialCardView materialCardView = view.findViewById(R.id.card_main_text);
         materialCardView.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +56,22 @@ public class MainFragment extends Fragment {
                 transaction.commit();
             }
         });
+
+        NavigationView navigationView=view.findViewById(R.id.navigationView_main);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.menuItemHome:
+                        //todo
+                }
+                return false;
+            }
+        });
+
+        View headerView=navigationView.getHeaderView(0);
+        TextView textView=headerView.findViewById(R.id.textView_header);
+        textView.setText(R.string.app_name);
 
         FloatingActionButton floatingActionButton = view.findViewById(R.id.fab_main);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
